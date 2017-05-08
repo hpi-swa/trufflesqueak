3f3c444da67cf189e393456cdf8a17ff8a0c7f13
Tim Felgentreff <timfelgentreff@gmail.com>
2017-05-08 15:24:08 +0200

    add type system

--- trufflesqueak/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/SqueakNode.java
+++ trufflesqueak/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/SqueakNode.java
@@ -1,11 +1,13 @@
 package de.hpi.swa.trufflesqueak.nodes;
 
+import com.oracle.truffle.api.dsl.TypeSystemReference;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.LoopNode;
 import com.oracle.truffle.api.nodes.Node;
 
 import de.hpi.swa.trufflesqueak.nodes.bytecodes.jump.LoopRepeatingNode;
 
+@TypeSystemReference(SqueakTypes.class)
 public abstract class SqueakNode extends Node {
     public abstract Object executeGeneric(VirtualFrame frame);
 
