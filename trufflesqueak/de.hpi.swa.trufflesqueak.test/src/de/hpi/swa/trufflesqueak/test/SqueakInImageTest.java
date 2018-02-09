1d8038c23d16f06449b121c047f73d5876a114c5
Fabio Niephaus <code@fniephaus.com>
2018-02-09 01:02:54 +0100

    Extend and improve in-image tests

--- trufflesqueak/de.hpi.swa.trufflesqueak.test/src/de/hpi/swa/trufflesqueak/test/SqueakInImageTest.java
+++ trufflesqueak/de.hpi.swa.trufflesqueak.test/src/de/hpi/swa/trufflesqueak/test/SqueakInImageTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertNotEquals;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
@@ -24,6 +26,105 @@ public class SqueakInImageTest extends AbstractSqueakTestCase {
     private static Object smalltalkAssociation;
     private static Object evaluateSymbol;
     private static Object compilerSymbol;
+
+    private static final Pattern runPattern = Pattern.compile("([0-9]+)\\ run");
+    private static final Pattern passesPattern = Pattern.compile("([0-9]+)\\ passes");
+
+    @Test
+    public void testAAsSymbol() {
+        assertEquals(image.asSymbol, asSymbol("asSymbol"));
+    }
+
+    @Test
+    public void testBNumerical() {
+        // Evaluate a few simple expressions to ensure that methodDictionaries grow correctly.
+        for (long i = 0; i < 10; i++) {
+            assertEquals(i + 1, evaluate(i + " + 1"));
+        }
+        assertEquals(4L, evaluate("-1 \\\\ 5"));
+    }
+
+    @Test
+    public void testCThisContext() {
+        assertEquals(42L, evaluate("thisContext return: 42"));
+    }
+
+    @Test
+    public void testDEnsure() {
+        assertEquals(21L, evaluate("[21] ensure: [42]"));
+        assertEquals(42L, evaluate("[21] ensure: [^42]"));
+        assertEquals(21L, evaluate("[^21] ensure: [42]"));
+        assertEquals(42L, evaluate("[^21] ensure: [^42]"));
+    }
+
+    @Test
+    public void testEOnError() {
+        Object result = evaluate("[self error: 'foobar'] on: Error do: [:err| ^ err messageText]");
+        assertEquals("foobar", result.toString());
+        assertEquals("foobar", evaluate("[[self error: 'foobar'] value] on: Error do: [:err| ^ err messageText]").toString());
+        assertEquals(image.sqTrue, evaluate("[[self error: 'foobar'] on: ZeroDivide do: [:e|]] on: Error do: [:err| ^ true]"));
+        assertEquals(image.sqTrue, evaluate("[self error: 'foobar'. false] on: Error do: [:err| ^ err return: true]"));
+    }
+
+    @Test
+    public void testFValue() {
+        assertEquals(42L, evaluate("[42] value"));
+        assertEquals(21L, evaluate("[[21] value] value"));
+    }
+
+    @Test
+    public void testGSUnitTest() {
+        assertEquals(image.sqTrue, evaluate("(TestCase new should: [1/0] raise: ZeroDivide) isKindOf: TestCase"));
+    }
+
+    @Test
+    public void testWTinyBenchmarks() {
+        String resultString = evaluate("1 tinyBenchmarks").toString();
+        assertTrue(resultString.contains("bytecodes/sec"));
+        assertTrue(resultString.contains("sends/sec"));
+    }
+
+    @Test
+    public void testXPassingSqueakTests() {
+        String[] testClasses = {"BagTest", "BooleanTest", "CollectionTest", "ProtoObjectTest", "SetTest", "UndefinedObjectTest"};
+        for (int i = 0; i < testClasses.length; i++) {
+            String testClass = testClasses[i];
+            if (!evaluate(testClass + " buildSuite run hasPassed").equals(image.sqTrue)) {
+                fail(testClass + " failed");
+            }
+        }
+    }
+
+    @Test
+    public void testYFailingSqueakTests() {
+        String[] testClasses = {"ArrayTest", "DateTest", "DictionaryTest", "DependentsArrayTest", "FalseTest", "LinkedListTest", "ObjectTest", "SmallIntegerTest",
+                        "StringTest", "SymbolTest"};
+        for (int i = 0; i < testClasses.length; i++) {
+            String testClass = testClasses[i];
+            image.getOutput().print(testClass + ": ");
+            String resultString = evaluate(testClass + " buildSuite run asString").toString();
+            image.getOutput().println(resultString);
+            Matcher runMatcher = runPattern.matcher(resultString);
+            Matcher passesMatcher = passesPattern.matcher(resultString);
+            if (runMatcher.find() && passesMatcher.find()) {
+                if (runMatcher.group(1).equals(passesMatcher.group(1))) {
+                    fail(testClass + " was failing but appears to be passing now");
+                }
+            } else {
+                fail("Unable to find number of runs or/and number of passes");
+            }
+        }
+    }
+
+    @Ignore
+    @Test
+    public void testZBrokenSqueakTests() {
+        evaluate("ClosureTest buildSuite run");
+        evaluate("FloatTest buildSuite run");
+        evaluate("NumberTest buildSuite run");
+        evaluate("ProcessTest buildSuite run");
+        evaluate("TrueTest buildSuite run"); // doesNotUnderstand not working yet
+    }
 
     @BeforeClass
     public static void setUpSqueakImageContext() {
@@ -91,88 +192,5 @@ public class SqueakInImageTest extends AbstractSqueakTestCase {
                         new Object[]{6L, getEvaluateSymbol(), getSmalltalkAssociation(), getCompilerSymbol(), image.wrap(expression), asSymbol(fakeMethodName), getSmalltalkAssociation()},
                         new int[]{0x41, 0x22, 0xC0, 0x23, 0xE0, 0x7C});
         return runMethod(method, getSmalltalkDictionary());
-    }
-
-    @Test
-    public void testAsSymbol() {
-        assertEquals(image.asSymbol, asSymbol("asSymbol"));
-    }
-
-    @Test
-    public void testNumerical() {
-        // Evaluate a few simple expressions to ensure that methodDictionaries grow correctly.
-        for (long i = 0; i < 10; i++) {
-            assertEquals(i + 1, evaluate(i + " + 1"));
-        }
-        assertEquals(4L, evaluate("-1 \\\\ 5"));
-    }
-
-    @Test
-    public void testThisContext() {
-        assertEquals(42L, evaluate("thisContext return: 42"));
-    }
-
-    @Test
-    public void testEnsure() {
-        assertEquals(21L, evaluate("[21] ensure: [42]"));
-        assertEquals(42L, evaluate("[21] ensure: [^42]"));
-        assertEquals(21L, evaluate("[^21] ensure: [42]"));
-        assertEquals(42L, evaluate("[^21] ensure: [^42]"));
-    }
-
-    @Test
-    public void testOnError() {
-        Object result = evaluate("[self error: 'foobar'] on: Error do: [:err| ^ err messageText]");
-        assertEquals("foobar", result.toString());
-        assertEquals("foobar", evaluate("[[self error: 'foobar'] value] on: Error do: [:err| ^ err messageText]").toString());
-        assertEquals(image.sqTrue, evaluate("[[self error: 'foobar'] on: ZeroDivide do: [:e|]] on: Error do: [:err| ^ true]"));
-        assertEquals(image.sqTrue, evaluate("[self error: 'foobar'. false] on: Error do: [:err| ^ err return: true]"));
-    }
-
-    @Test
-    public void testValue() {
-        assertEquals(42L, evaluate("[42] value"));
-        assertEquals(21L, evaluate("[[21] value] value"));
-    }
-
-    @Test
-    public void testTinyBenchmarks() {
-        String resultString = evaluate("1 tinyBenchmarks").toString();
-        assertTrue(resultString.contains("bytecodes/sec"));
-        assertTrue(resultString.contains("sends/sec"));
-    }
-
-    @Test
-    public void testSUnitTest() {
-        assertEquals(image.sqTrue, evaluate("(TestCase new should: [1/0] raise: ZeroDivide) isKindOf: TestCase"));
-    }
-
-    @Test
-    public void testRunArrayTest() {
-        // Run all tests of ArrayTest
-        // String resultString = evaluate("ArrayTest buildSuite run").toString();
-
-        // Run selected tests
-        Object result = evaluate("#(#testAtWrap #testEmpty #testIsArray #testIsLiteral #testLiteralEqual #testNewWithSize) allSatisfy: [:ea | (ArrayTest run: ea) hasPassed]");
-        assertEquals(image.sqTrue, result);
-    }
-
-    @Test
-    public void testRunSmallIntegerTest() {
-        // Run all tests of SmallIntegerTest
-        assertEquals(image.sqTrue, evaluate("SmallIntegerTest buildSuite run hasPassed"));
-    }
-
-    @Ignore
-    @Test
-    public void testRunFailingTests() {
-        assertEquals(image.sqTrue, evaluate("BooleanTest buildSuite run hasPassed"));
-        assertEquals(image.sqTrue, evaluate("DependentsArrayTest buildSuite run defects asString"));
-        assertEquals(image.sqTrue, evaluate("FalseTest buildSuite run defects asString"));
-        assertEquals(image.sqTrue, evaluate("ObjectTest buildSuite run hasPassed"));
-        assertEquals(image.sqTrue, evaluate("ProtoObjectTest buildSuite run defects asString"));
-        assertEquals(image.sqTrue, evaluate("TrueTest buildSuite run defects asString"));
-        assertEquals(image.sqTrue, evaluate("UndefinedObjectTest buildSuite run defects asString"));
-        assertEquals(image.sqTrue, evaluate("WeakMessageSendTest buildSuite run defects asString"));
     }
 }
