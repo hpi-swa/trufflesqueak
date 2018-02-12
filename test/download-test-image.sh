e6ec637e7ac8b2b95fd829a2f101b809c2423892
Fabio Niephaus <code@fniephaus.com>
2018-02-12 19:30:43 +0100

    Silence wget output when downloading test image

--- test/download-test-image.sh
+++ test/download-test-image.sh
@@ -5,7 +5,7 @@ readonly TARGET_DIR="${TRAVIS_BUILD_DIR}/images"
 
 mkdir "${TARGET_DIR}" > /dev/null
 pushd "${TARGET_DIR}"
-wget https://www.hpi.uni-potsdam.de/hirschfeld/artifacts/trufflesqueak/Squeak6.0alpha-17606-32bit.zip
+wget -q https://www.hpi.uni-potsdam.de/hirschfeld/artifacts/trufflesqueak/Squeak6.0alpha-17606-32bit.zip
 unzip Squeak6.0alpha-17606-32bit.zip
 mv *.image test.image
 mv *.changes test.changes
