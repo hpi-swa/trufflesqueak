b7cd63c5b66fc1ba2532487e40438df8ae08a2b8
Fabio Niephaus <code@fniephaus.com>
2018-02-13 12:54:52 +0100

    Add StackOverflowError protection

--- trufflesqueak/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/InvokeNode.java
+++ trufflesqueak/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/InvokeNode.java
@@ -7,9 +7,12 @@ import com.oracle.truffle.api.nodes.DirectCallNode;
 import com.oracle.truffle.api.nodes.IndirectCallNode;
 import com.oracle.truffle.api.nodes.Node;
 
+import de.hpi.swa.trufflesqueak.exceptions.SqueakException;
 import de.hpi.swa.trufflesqueak.model.CompiledCodeObject;
 
 public abstract class InvokeNode extends Node {
+    public static int callDepth = 0;
+    private final static int CALL_DEPTH_LIMIT = 400;
 
     public static InvokeNode create() {
         return InvokeNodeGen.create();
@@ -21,12 +24,29 @@ public abstract class InvokeNode extends Node {
     protected Object doInvoke(@SuppressWarnings("unused") CompiledCodeObject code, Object[] arguments,
                     @SuppressWarnings("unused") @Cached("code.getCallTarget()") RootCallTarget callTarget,
                     @Cached("create(callTarget)") DirectCallNode callNode) {
-        return callNode.call(arguments);
+        incrementAndCheckCallDepth();
+        try {
+            return callNode.call(arguments);
+        } finally {
+            callDepth--;
+        }
     }
 
     @Specialization(replaces = "doInvoke")
     protected Object doIndirect(CompiledCodeObject code, Object[] arguments,
                     @Cached("create()") IndirectCallNode callNode) {
-        return callNode.call(code.getCallTarget(), arguments);
+        incrementAndCheckCallDepth();
+        try {
+            return callNode.call(code.getCallTarget(), arguments);
+        } finally {
+            callDepth--;
+        }
+    }
+
+    private static void incrementAndCheckCallDepth() {
+        callDepth++;
+        if (callDepth > CALL_DEPTH_LIMIT) {
+            throw new SqueakException("Avoiding StackOverflowError, callDepth has reached " + CALL_DEPTH_LIMIT + ".");
+        }
     }
 }
