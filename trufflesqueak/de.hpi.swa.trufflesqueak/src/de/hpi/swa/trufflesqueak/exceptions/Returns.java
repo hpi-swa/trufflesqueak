38f9fd942da5b171331960412e0bebc1a2d9e584
Fabio Niephaus <code@fniephaus.com>
2018-04-19 16:04:29 +0200

    Mark classes, methods, and arguments as final

--- trufflesqueak/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/exceptions/Returns.java
+++ trufflesqueak/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/exceptions/Returns.java
@@ -1,5 +1,6 @@
 package de.hpi.swa.trufflesqueak.exceptions;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.nodes.ControlFlowException;
 
@@ -7,115 +8,116 @@ import de.hpi.swa.trufflesqueak.model.ContextObject;
 
 public final class Returns {
     private static abstract class AbstractReturn extends ControlFlowException {
-        private static final long serialVersionUID = 1L;
+        @CompilationFinal private static final long serialVersionUID = 1L;
         @CompilationFinal protected final Object returnValue;
 
-        public AbstractReturn(Object result) {
+        public AbstractReturn(final Object result) {
             returnValue = result;
         }
 
-        public Object getReturnValue() {
+        public final Object getReturnValue() {
             return returnValue;
         }
     }
 
-    public static class FreshReturn extends ControlFlowException {
-        private static final long serialVersionUID = 1L;
+    public static final class FreshReturn extends ControlFlowException {
+        @CompilationFinal private static final long serialVersionUID = 1L;
         @CompilationFinal protected final AbstractReturn returnValue;
 
-        public FreshReturn(AbstractReturn result) {
+        public FreshReturn(final AbstractReturn result) {
             returnValue = result;
         }
 
-        public AbstractReturn getReturnException() {
+        public final AbstractReturn getReturnException() {
             return returnValue;
         }
 
         @Override
-        public String toString() {
+        public final String toString() {
             return "Fresh (value: " + returnValue + ")";
         }
     }
 
-    public static class LocalReturn extends AbstractReturn {
-        private static final long serialVersionUID = 1L;
+    public static final class LocalReturn extends AbstractReturn {
+        @CompilationFinal private static final long serialVersionUID = 1L;
 
         public LocalReturn(Object result) {
             super(result);
         }
 
         @Override
-        public String toString() {
+        public final String toString() {
             return "LR (value: " + returnValue + ")";
         }
     }
 
-    public static class NonLocalReturn extends AbstractReturn {
-        private static final long serialVersionUID = 1L;
+    public static final class NonLocalReturn extends AbstractReturn {
+        @CompilationFinal private static final long serialVersionUID = 1L;
         @CompilationFinal private ContextObject targetContext;
-        private boolean arrivedAtTargetContext = false;
+        @CompilationFinal private boolean arrivedAtTargetContext = false;
 
-        public NonLocalReturn(Object returnValue, ContextObject targetContext) {
+        public NonLocalReturn(final Object returnValue, final ContextObject targetContext) {
             super(returnValue);
             this.targetContext = targetContext;
         }
 
-        public ContextObject getTargetContext() {
+        public final ContextObject getTargetContext() {
             return targetContext;
         }
 
-        public boolean hasArrivedAtTargetContext() {
+        public final boolean hasArrivedAtTargetContext() {
             return arrivedAtTargetContext;
         }
 
-        public void setArrivedAtTargetContext() {
+        public final void setArrivedAtTargetContext() {
+            CompilerDirectives.transferToInterpreterAndInvalidate();
             arrivedAtTargetContext = true;
         }
 
         @Override
-        public String toString() {
+        public final String toString() {
             return "NLR (value: " + returnValue + ", arrived: " + arrivedAtTargetContext + ", target: " + targetContext + ")";
         }
     }
 
-    public static class NonVirtualContextModification extends Exception {
-        private static final long serialVersionUID = 1L;
+    public static final class NonVirtualContextModification extends Exception {
+        @CompilationFinal private static final long serialVersionUID = 1L;
     }
 
-    public static class NonVirtualReturn extends AbstractReturn {
-        private static final long serialVersionUID = 1L;
+    public static final class NonVirtualReturn extends AbstractReturn {
+        @CompilationFinal private static final long serialVersionUID = 1L;
         @CompilationFinal private final ContextObject targetContext;
         @CompilationFinal private final ContextObject currentContext;
 
-        public NonVirtualReturn(Object returnValue, ContextObject targetContext, ContextObject currentContext) {
+        public NonVirtualReturn(final Object returnValue, final ContextObject targetContext, final ContextObject currentContext) {
             super(returnValue);
             this.targetContext = targetContext;
             this.currentContext = currentContext;
         }
 
-        public ContextObject getTargetContext() {
+        public final ContextObject getTargetContext() {
             return targetContext;
         }
 
-        public ContextObject getCurrentContext() {
+        public final ContextObject getCurrentContext() {
             return currentContext;
         }
 
         @Override
-        public String toString() {
+        public final String toString() {
             return "NVR (value: " + returnValue + ", current: " + currentContext + ", target: " + targetContext + ")";
         }
     }
 
     public static class TopLevelReturn extends AbstractReturn {
-        private static final long serialVersionUID = 1L;
+        @CompilationFinal private static final long serialVersionUID = 1L;
 
-        public TopLevelReturn(Object result) {
+        public TopLevelReturn(final Object result) {
             super(result);
         }
 
         @Override
-        public String toString() {
+        public final String toString() {
             return "TLR (value: " + returnValue + ")";
         }
     }
