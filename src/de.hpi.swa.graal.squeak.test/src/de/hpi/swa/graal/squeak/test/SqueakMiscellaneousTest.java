99097f53d55d9885040c9fae77f9c368a1b290a8
Fabio Niephaus <code@fniephaus.com>
2018-04-24 18:05:36 +0200

    Refactor SqueakMiscellaneousTest

--- src/de.hpi.swa.graal.squeak.test/src/de/hpi/swa/graal/squeak/test/SqueakMiscellaneousTest.java
+++ src/de.hpi.swa.graal.squeak.test/src/de/hpi/swa/graal/squeak/test/SqueakMiscellaneousTest.java
@@ -15,11 +15,94 @@ import de.hpi.swa.graal.squeak.nodes.bytecodes.MiscellaneousBytecodes.DupNode;
 import de.hpi.swa.graal.squeak.nodes.bytecodes.MiscellaneousBytecodes.PopNode;
 import de.hpi.swa.graal.squeak.nodes.bytecodes.PushBytecodes.PushConstantNode;
 import de.hpi.swa.graal.squeak.nodes.bytecodes.ReturnBytecodes.ReturnReceiverNode;
+import de.hpi.swa.graal.squeak.util.AbstractImageChunk;
 import de.hpi.swa.graal.squeak.util.SqueakBytecodeDecoder;
 import de.hpi.swa.graal.squeak.util.SqueakImageChunk;
-import de.hpi.swa.graal.squeak.util.AbstractImageChunk;
 
 public class SqueakMiscellaneousTest extends AbstractSqueakTestCaseWithDummyImage {
+    private static final String ALL_BYTECODES_EXPECTED_RESULT = String.join("\n", "1 <0F> pushRcvr: 15",
+                    "2 <1F> pushTemp: 15",
+                    "3 <3F> pushConstant: 17104899",
+                    "4 <5F> pushLit: 31",
+                    "5 <60> popIntoRcvr: 0",
+                    "6 <61> popIntoRcvr: 1",
+                    "7 <62> popIntoRcvr: 2",
+                    "8 <63> popIntoRcvr: 3",
+                    "9 <67> popIntoRcvr: 7",
+                    "10 <6F> popIntoTemp: 7",
+                    "11 <70> self",
+                    "12 <71> pushConstant: true",
+                    "13 <72> pushConstant: false",
+                    "14 <73> pushConstant: nil",
+                    "15 <74> pushConstant: -1",
+                    "16 <75> pushConstant: 0",
+                    "17 <76> pushConstant: 1",
+                    "18 <77> pushConstant: 2",
+                    "19 <78> returnSelf",
+                    "20 <79> return: true",
+                    "21 <7A> return: false",
+                    "22 <7B> return: nil",
+                    "23 <7C> returnTop",
+                    "24 <7E> unknown: 126",
+                    "25 <7F> unknown: 127",
+                    "26 <80 1F> pushRcvr: 31",
+                    "27 <81 1F> storeIntoRcvr: 31",
+                    "28 <82 1F> popIntoRcvr: 31",
+                    "29 <83 1F> send: 17104899",
+                    "30 <84 1F 3F> send: 17104899",
+                    "31 <85 1F> sendSuper: 17104899",
+                    "32 <86 1F> send: 17104899",
+                    "33 <87> pop",
+                    "34 <88> dup",
+                    "35 <89> pushThisContext:",
+                    "36 <8A 1F> push: (Array new: 31)",
+                    "37 <8B 1F 00> callPrimitive: 31",
+                    "38 <8C 1F 3F> pushTemp: 31 inVectorAt: 63",
+                    "39 <8D 1F 3F> storeIntoTemp: 31 inVectorAt: 63",
+                    "40 <8E 1F 3F> popIntoTemp: 31 inVectorAt: 63",
+                    "41 <8F 1F 3F 7F> closureNumCopied: 1 numArgs: 15 bytes 61 to 16316",
+                    "42  <97> jumpTo: 8",
+                    "43  <9F> jumpFalse: 8",
+                    "44  <A7 1F> jumpTo: 799",
+                    "45  <AB 1F> jumpTrue: 799",
+                    "46  <AF 1F> jumpFalse: 799",
+                    "47  <7D> blockReturn",
+                    "48 <B0> send: plus",
+                    "49 <B1> send: minus",
+                    "50 <B2> send: lt",
+                    "51 <B3> send: gt",
+                    "52 <B4> send: le",
+                    "53 <B5> send: ge",
+                    "54 <B6> send: eq",
+                    "55 <B7> send: ne",
+                    "56 <B8> send: times",
+                    "57 <B9> send: divide",
+                    "58 <BA> send: modulo",
+                    "59 <BB> send: pointAt",
+                    "60 <BC> send: bitShift",
+                    "61 <BD> send: floorDivide",
+                    "62 <BE> send: bitAnd",
+                    "63 <BF> send: bitOr",
+                    "64 <C0> send: at",
+                    "65 <C1> send: atput",
+                    "66 <C2> send: size",
+                    "67 <C3> send: next",
+                    "68 <C4> send: nextPut",
+                    "69 <C5> send: atEnd",
+                    "70 <C6> send: equivalent",
+                    "71 <C7> send: klass",
+                    "72 <C8> send: blockCopy",
+                    "73 <C9> send: value",
+                    "74 <CA> send: valueWithArg",
+                    "75 <CB> send: do",
+                    "76 <CC> send: new",
+                    "77 <CD> send: newWithArg",
+                    "78 <CE> send: x",
+                    "79 <CF> send: y",
+                    "80 <D0> send: 21",
+                    "81 <E1> send: 42",
+                    "82 <F2> send: 63");
+
     @Test
     public void testIfNil() {
         // (1 ifNil: [true]) class
@@ -51,137 +134,50 @@ public class SqueakMiscellaneousTest extends AbstractSqueakTestCaseWithDummyImag
                                                                                  // numTemp=55
         final CompiledCodeObject code = makeMethod(literals, 0x70, 0x68, 0x10, 0x8F, 0x10, 0x00, 0x02, 0x10, 0x7D, 0xC9, 0x7C);
         final CharSequence source = CompiledCodeObjectPrinter.getString(code);
-        //@formatter:off
-        assertEquals(
-            "1 <70> self\n" +
-            "2 <68> popIntoTemp: 0\n" +
-            "3 <10> pushTemp: 0\n" +
-            "4 <8F 10 00 02> closureNumCopied: 1 numArgs: 0 bytes 7 to 9\n" +
-            "5  <10> pushTemp: 0\n" +
-            "6  <7D> blockReturn\n" +
-            "7 <C9> send: value\n" +
-            "8 <7C> returnTop", source);
-        //@formatter:on
+        assertEquals(String.join("\n",
+                        "1 <70> self",
+                        "2 <68> popIntoTemp: 0",
+                        "3 <10> pushTemp: 0",
+                        "4 <8F 10 00 02> closureNumCopied: 1 numArgs: 0 bytes 7 to 9",
+                        "5  <10> pushTemp: 0",
+                        "6  <7D> blockReturn",
+                        "7 <C9> send: value",
+                        "8 <7C> returnTop"), source);
     }
 
     @Test
     public void testSourceAllBytecodes() {
         final Object[] literals = new Object[]{17104899L, 21, 42, 63};
         final CompiledCodeObject code = makeMethod(literals,
-        //@formatter:off
-            15, 31, 63, 95, 96, 97, 98, 99, 103, 111, 112, 113, 114, 115, 116,
-            117, 118, 119, 120, 121, 122, 123, 124, 126, 127,
-            128, 31,
-            129, 31,
-            130, 31,
-            131, 31,
-            132, 31, 63,
-            133, 31,
-            134, 31,
-            135,
-            136,
-            137,
-            138, 31,
-            139, 31, 0,
-            140, 31, 63,
-            141, 31, 63,
-            142, 31, 63,
-            143, 31, 63, 127,
-            151,
-            159,
-            167, 31,
-            171, 31,
-            175, 31,
-            125,
-            176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188,
-            189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201,
-            202, 203, 204, 205, 206, 207, 208, 225, 242
-        );
+                        15, 31, 63, 95, 96, 97, 98, 99, 103, 111, 112, 113, 114, 115, 116,
+                        117, 118, 119, 120, 121, 122, 123, 124, 126, 127,
+                        128, 31,
+                        129, 31,
+                        130, 31,
+                        131, 31,
+                        132, 31, 63,
+                        133, 31,
+                        134, 31,
+                        135,
+                        136,
+                        137,
+                        138, 31,
+                        139, 31, 0,
+                        140, 31, 63,
+                        141, 31, 63,
+                        142, 31, 63,
+                        143, 31, 63, 127,
+                        151,
+                        159,
+                        167, 31,
+                        171, 31,
+                        175, 31,
+                        125,
+                        176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188,
+                        189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201,
+                        202, 203, 204, 205, 206, 207, 208, 225, 242);
         final CharSequence source = CompiledCodeObjectPrinter.getString(code);
-        assertEquals(
-            "1 <0F> pushRcvr: 15\n" +
-            "2 <1F> pushTemp: 15\n" +
-            "3 <3F> pushConstant: 17104899\n" +
-            "4 <5F> pushLit: 31\n" +
-            "5 <60> popIntoRcvr: 0\n" +
-            "6 <61> popIntoRcvr: 1\n" +
-            "7 <62> popIntoRcvr: 2\n" +
-            "8 <63> popIntoRcvr: 3\n" +
-            "9 <67> popIntoRcvr: 7\n" +
-            "10 <6F> popIntoTemp: 7\n" +
-            "11 <70> self\n" +
-            "12 <71> pushConstant: true\n" +
-            "13 <72> pushConstant: false\n" +
-            "14 <73> pushConstant: nil\n" +
-            "15 <74> pushConstant: -1\n" +
-            "16 <75> pushConstant: 0\n" +
-            "17 <76> pushConstant: 1\n" +
-            "18 <77> pushConstant: 2\n" +
-            "19 <78> returnSelf\n" +
-            "20 <79> return: true\n" +
-            "21 <7A> return: false\n" +
-            "22 <7B> return: nil\n" +
-            "23 <7C> returnTop\n" +
-            "24 <7E> unknown: 126\n" +
-            "25 <7F> unknown: 127\n" +
-            "26 <80 1F> pushRcvr: 31\n" +
-            "27 <81 1F> storeIntoRcvr: 31\n" +
-            "28 <82 1F> popIntoRcvr: 31\n" +
-            "29 <83 1F> send: 17104899\n" +
-            "30 <84 1F 3F> send: 17104899\n" +
-            "31 <85 1F> sendSuper: 17104899\n" +
-            "32 <86 1F> send: 17104899\n" +
-            "33 <87> pop\n" +
-            "34 <88> dup\n" +
-            "35 <89> pushThisContext:\n" +
-            "36 <8A 1F> push: (Array new: 31)\n" +
-            "37 <8B 1F 00> callPrimitive: 31\n" +
-            "38 <8C 1F 3F> pushTemp: 31 inVectorAt: 63\n" +
-            "39 <8D 1F 3F> storeIntoTemp: 31 inVectorAt: 63\n" +
-            "40 <8E 1F 3F> popIntoTemp: 31 inVectorAt: 63\n" +
-            "41 <8F 1F 3F 7F> closureNumCopied: 1 numArgs: 15 bytes 61 to 16316\n" +
-            "42  <97> jumpTo: 8\n" +
-            "43  <9F> jumpFalse: 8\n" +
-            "44  <A7 1F> jumpTo: 799\n" +
-            "45  <AB 1F> jumpTrue: 799\n" +
-            "46  <AF 1F> jumpFalse: 799\n" +
-            "47  <7D> blockReturn\n" +
-            "48 <B0> send: plus\n" +
-            "49 <B1> send: minus\n" +
-            "50 <B2> send: lt\n" +
-            "51 <B3> send: gt\n" +
-            "52 <B4> send: le\n" +
-            "53 <B5> send: ge\n" +
-            "54 <B6> send: eq\n" +
-            "55 <B7> send: ne\n" +
-            "56 <B8> send: times\n" +
-            "57 <B9> send: divide\n" +
-            "58 <BA> send: modulo\n" +
-            "59 <BB> send: pointAt\n" +
-            "60 <BC> send: bitShift\n" +
-            "61 <BD> send: floorDivide\n" +
-            "62 <BE> send: bitAnd\n" +
-            "63 <BF> send: bitOr\n" +
-            "64 <C0> send: at\n" +
-            "65 <C1> send: atput\n" +
-            "66 <C2> send: size\n" +
-            "67 <C3> send: next\n" +
-            "68 <C4> send: nextPut\n" +
-            "69 <C5> send: atEnd\n" +
-            "70 <C6> send: equivalent\n" +
-            "71 <C7> send: klass\n" +
-            "72 <C8> send: blockCopy\n" +
-            "73 <C9> send: value\n" +
-            "74 <CA> send: valueWithArg\n" +
-            "75 <CB> send: do\n" +
-            "76 <CC> send: new\n" +
-            "77 <CD> send: newWithArg\n" +
-            "78 <CE> send: x\n" +
-            "79 <CF> send: y\n" +
-            "80 <D0> send: 21\n" +
-            "81 <E1> send: 42\n" +
-            "82 <F2> send: 63", source);
-        //@formatter:on
+        assertEquals(ALL_BYTECODES_EXPECTED_RESULT, source);
     }
 
     @Test
