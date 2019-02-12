e1bb7897dba916c645460afd2c1038a0a6b896d7
Fabio Niephaus <code@fniephaus.com>
2019-02-12 10:48:26 +0100

    Improve LookupClassNode and use smallFloatClass

--- src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/context/LookupClassNode.java
+++ src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/context/LookupClassNode.java
@@ -6,10 +6,19 @@ import com.oracle.truffle.api.interop.TruffleObject;
 
 import de.hpi.swa.graal.squeak.exceptions.SqueakExceptions.SqueakException;
 import de.hpi.swa.graal.squeak.image.SqueakImageContext;
-import de.hpi.swa.graal.squeak.model.AbstractSqueakObject;
+import de.hpi.swa.graal.squeak.model.AbstractPointersObject;
+import de.hpi.swa.graal.squeak.model.ArrayObject;
+import de.hpi.swa.graal.squeak.model.BlockClosureObject;
 import de.hpi.swa.graal.squeak.model.CharacterObject;
 import de.hpi.swa.graal.squeak.model.ClassObject;
+import de.hpi.swa.graal.squeak.model.CompiledBlockObject;
+import de.hpi.swa.graal.squeak.model.CompiledMethodObject;
+import de.hpi.swa.graal.squeak.model.ContextObject;
+import de.hpi.swa.graal.squeak.model.EmptyObject;
+import de.hpi.swa.graal.squeak.model.FloatObject;
 import de.hpi.swa.graal.squeak.model.LargeIntegerObject;
+import de.hpi.swa.graal.squeak.model.NativeObject;
+import de.hpi.swa.graal.squeak.model.NilObject;
 import de.hpi.swa.graal.squeak.nodes.AbstractNodeWithImage;
 
 public abstract class LookupClassNode extends AbstractNodeWithImage {
@@ -54,18 +63,74 @@ public abstract class LookupClassNode extends AbstractNodeWithImage {
     }
 
     @Specialization
-    protected final ClassObject doChar(@SuppressWarnings("unused") final CharacterObject value) {
+    protected final ClassObject doDouble(@SuppressWarnings("unused") final double value) {
+        assert image.flags.is64bit() : "Unexpected double value. 32-bit image does not support immediate floats.";
+        return image.smallFloatClass;
+    }
+
+    @Specialization
+    protected static final ClassObject doAbstractPointers(final AbstractPointersObject value) {
+        return value.getSqueakClass();
+    }
+
+    @Specialization
+    protected static final ClassObject doArray(final ArrayObject value) {
+        return value.getSqueakClass();
+    }
+
+    @Specialization
+    protected final ClassObject doClosure(@SuppressWarnings("unused") final BlockClosureObject value) {
+        return image.blockClosureClass;
+    }
+
+    @Specialization
+    protected final ClassObject doCharacter(@SuppressWarnings("unused") final CharacterObject value) {
         return image.characterClass;
     }
 
     @Specialization
-    protected final ClassObject doDouble(@SuppressWarnings("unused") final double value) {
+    protected static final ClassObject doClass(@SuppressWarnings("unused") final ClassObject value) {
+        return value.getSqueakClass();
+    }
+
+    @Specialization
+    protected static final ClassObject doBlock(@SuppressWarnings("unused") final CompiledBlockObject value) {
+        throw SqueakException.create("Should never happen?");
+    }
+
+    @Specialization
+    protected final ClassObject doMethod(@SuppressWarnings("unused") final CompiledMethodObject value) {
+        return image.compiledMethodClass;
+    }
+
+    @Specialization
+    protected final ClassObject doContext(@SuppressWarnings("unused") final ContextObject value) {
+        return image.methodContextClass;
+    }
+
+    @Specialization
+    protected static final ClassObject doEmpty(final EmptyObject value) {
+        return value.getSqueakClass();
+    }
+
+    @Specialization
+    protected final ClassObject doFloat(@SuppressWarnings("unused") final FloatObject value) {
         return image.floatClass;
     }
 
     @Specialization
-    protected static final ClassObject doSqueakObject(final AbstractSqueakObject value) {
+    protected static final ClassObject doLargeInteger(final LargeIntegerObject value) {
         return value.getSqueakClass();
+    }
+
+    @Specialization
+    protected static final ClassObject doNative(final NativeObject value) {
+        return value.getSqueakClass();
+    }
+
+    @Specialization
+    protected final ClassObject doNil(@SuppressWarnings("unused") final NilObject value) {
+        return image.nilClass;
     }
 
     @Specialization(guards = {"!isAbstractSqueakObject(value)"})
