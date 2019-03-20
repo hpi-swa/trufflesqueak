8a9883ea685acd153cfbeac5b9396ce18cdd727f
Fabio Niephaus <code@fniephaus.com>
2019-03-21 00:15:05 +0100

    Fix compilation issues

--- src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/context/LookupClassNodeInterface.java
+++ src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/context/LookupClassNodeInterface.java
@@ -1,7 +1,9 @@
 package de.hpi.swa.graal.squeak.nodes.context;
 
+import com.oracle.truffle.api.nodes.NodeInterface;
+
 import de.hpi.swa.graal.squeak.model.ClassObject;
 
-public interface LookupClassNodeInterface {
+public interface LookupClassNodeInterface extends NodeInterface {
     ClassObject executeLookup(Object receiver);
 }
