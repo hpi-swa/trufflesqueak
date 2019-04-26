52b9cb291af4d8d2921e814fe1d057db9dc7ae0d
Fabio Niephaus <code@fniephaus.com>
2019-04-26 18:14:35 +0200

    Remove isLargeInteger type check
    It is not needed and performs a redundant check

--- src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/SqueakArithmeticTypes.java
+++ src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/SqueakArithmeticTypes.java
@@ -1,21 +1,14 @@
 package de.hpi.swa.graal.squeak.nodes;
 
 import com.oracle.truffle.api.dsl.ImplicitCast;
-import com.oracle.truffle.api.dsl.TypeCheck;
 import com.oracle.truffle.api.dsl.TypeSystem;
 
 import de.hpi.swa.graal.squeak.model.FloatObject;
-import de.hpi.swa.graal.squeak.model.LargeIntegerObject;
 
 @TypeSystem
 public class SqueakArithmeticTypes {
-    @TypeCheck(LargeIntegerObject.class)
-    public static boolean isLargeInteger(final Object object) {
-        return object instanceof LargeIntegerObject && ((LargeIntegerObject) object).isLargeInteger();
-    }
-
     @ImplicitCast
-    public static double fromFloatObject(final FloatObject object) {
+    public static final double fromFloatObject(final FloatObject object) {
         return object.getValue();
     }
 }
