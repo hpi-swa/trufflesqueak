2044523f852d7de3720ba32dd86fa146ed3c4f01
Fabio Niephaus <code@fniephaus.com>
2019-05-07 10:37:54 +0200

    Use SqueakImageFilter in JFileChooser

--- src/de.hpi.swa.graal.squeak.launcher/src/de/hpi/swa/graal/squeak/launcher/FileChooser.java
+++ src/de.hpi.swa.graal.squeak.launcher/src/de/hpi/swa/graal/squeak/launcher/FileChooser.java
@@ -1,14 +1,31 @@
 package de.hpi.swa.graal.squeak.launcher;
 
+import java.io.File;
+
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
 
 public final class FileChooser {
     public static String run() {
         final JFileChooser squeakImageChooser = new JFileChooser();
+        squeakImageChooser.setFileFilter(new SqueakImageFilter());
+        squeakImageChooser.setApproveButtonToolTipText("Open selected image with GraalSqueak");
         final long result = squeakImageChooser.showOpenDialog(null);
         if (result == JFileChooser.APPROVE_OPTION) {
             return squeakImageChooser.getSelectedFile().getAbsolutePath();
         }
         return null;
+    }
+
+    private static class SqueakImageFilter extends FileFilter {
+        @Override
+        public boolean accept(final File f) {
+            return f.getName().endsWith(".image");
+        }
+
+        @Override
+        public String getDescription() {
+            return "Squeak/Smalltalk images";
+        }
     }
 }
