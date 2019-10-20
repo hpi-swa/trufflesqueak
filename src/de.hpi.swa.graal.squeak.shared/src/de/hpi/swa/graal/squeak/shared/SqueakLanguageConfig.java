f300d6360adcf044a1fbba7499f192bfeb422a43
Fabio Niephaus <code@fniephaus.com>
2019-10-20 14:27:09 +0300

    Bump version to 1.0.0-rc4

--- src/de.hpi.swa.graal.squeak.shared/src/de/hpi/swa/graal/squeak/shared/SqueakLanguageConfig.java
+++ src/de.hpi.swa.graal.squeak.shared/src/de/hpi/swa/graal/squeak/shared/SqueakLanguageConfig.java
@@ -11,5 +11,5 @@ public final class SqueakLanguageConfig {
     public static final String MIME_TYPE = "application/x-smalltalk";
     public static final String NAME = "Squeak/Smalltalk";
     public static final String ST_MIME_TYPE = "text/x-smalltalk";
-    public static final String VERSION = "1.0.0-rc3";
+    public static final String VERSION = "1.0.0-rc4";
 }
