fe0a559094696f2b118740c638b0e36bed500315
Florin Mateoc <fmateoc@yahoo.com>
2019-11-10 00:00:22 -0600

    fix a nasty bug in the interrupt handler node: the guards were referencing global mutable state, unrelated to the local code object for which supposedly the specialization was done. As a result, the global mutable value ended up being used in asserts in the specialization, asserts that would obviously sometimes fail. To fix it, remove the specialization, we don't have any grounds for it

--- src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/util/InterruptHandlerNode.java
+++ src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/util/InterruptHandlerNode.java
@@ -6,7 +6,6 @@
 package de.hpi.swa.graal.squeak.util;
 
 import com.oracle.truffle.api.TruffleLogger;
-import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.Node;
 
@@ -17,38 +16,35 @@ import de.hpi.swa.graal.squeak.nodes.primitives.impl.ControlPrimitives;
 import de.hpi.swa.graal.squeak.nodes.process.SignalSemaphoreNode;
 import de.hpi.swa.graal.squeak.shared.SqueakLanguageConfig;
 
-public abstract class InterruptHandlerNode extends Node {
+public final class InterruptHandlerNode extends Node {
     private static final TruffleLogger LOG = TruffleLogger.getLogger(SqueakLanguageConfig.ID, ControlPrimitives.class);
-    protected final Object[] specialObjects;
+    private final Object[] specialObjects;
     private final InterruptHandlerState istate;
 
     @Child private SignalSemaphoreNode signalSemaporeNode;
 
-    protected InterruptHandlerNode(final CompiledCodeObject code) {
+    private InterruptHandlerNode(final CompiledCodeObject code) {
         specialObjects = code.image.specialObjectsArray.getObjectStorage();
         istate = code.image.interrupt;
         signalSemaporeNode = SignalSemaphoreNode.create(code);
     }
 
     public static InterruptHandlerNode create(final CompiledCodeObject code) {
-        return InterruptHandlerNodeGen.create(code);
+        return new InterruptHandlerNode(code);
     }
 
-    public abstract void executeTrigger(VirtualFrame frame);
-
-    protected final boolean externalObjectsIsEmpty() {
-        return ((ArrayObject) specialObjects[SPECIAL_OBJECT.EXTERNAL_OBJECTS_ARRAY]).isEmptyType();
-    }
-
-    @Specialization(guards = {"!externalObjectsIsEmpty()"})
-    protected final void doFullCheck(final VirtualFrame frame) {
+    public void executeTrigger(final VirtualFrame frame) {
         performChecks(frame);
-        checkSemaphoresToSignal(frame);
-    }
-
-    @Specialization(guards = {"externalObjectsIsEmpty()"})
-    protected final void doCheck(final VirtualFrame frame) {
-        performChecks(frame);
+        final ArrayObject externalObjects = (ArrayObject) specialObjects[SPECIAL_OBJECT.EXTERNAL_OBJECTS_ARRAY];
+        if (!externalObjects.isEmptyType()) {
+            final Object[] semaphores = externalObjects.getObjectStorage();
+            Integer semaIndex;
+            while ((semaIndex = istate.nextSemaphoreToSignal()) != null) {
+                final Object semaphore = semaphores[semaIndex - 1];
+                LOG.fine(() -> "Signalling external semaphore @" + semaphore.hashCode() + " in interrupt handler");
+                signalSemaporeNode.executeSignal(frame, semaphore);
+            }
+        }
     }
 
     private void performChecks(final VirtualFrame frame) {
@@ -66,16 +62,6 @@ public abstract class InterruptHandlerNode extends Node {
             istate.setPendingFinalizations(false);
             LOG.fine(() -> "Signalling finalizations semaphore @" + specialObjects[SPECIAL_OBJECT.THE_FINALIZATION_SEMAPHORE].hashCode() + " in interrupt handler");
             signalSemaporeNode.executeSignal(frame, specialObjects[SPECIAL_OBJECT.THE_FINALIZATION_SEMAPHORE]);
-        }
-    }
-
-    private void checkSemaphoresToSignal(final VirtualFrame frame) {
-        final Object[] semaphores = ((ArrayObject) specialObjects[SPECIAL_OBJECT.EXTERNAL_OBJECTS_ARRAY]).getObjectStorage();
-        Integer semaIndex;
-        while ((semaIndex = istate.nextSemaphoreToSignal()) != null) {
-            final Object semaphore = semaphores[semaIndex - 1];
-            LOG.fine(() -> "Signalling external semaphore @" + semaphore.hashCode() + " in interrupt handler");
-            signalSemaporeNode.executeSignal(frame, semaphore);
         }
     }
 }
