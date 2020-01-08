371a0b17c82bb107611201242d7f503967cc6093
Fabio Niephaus <code@fniephaus.com>
2020-01-08 18:15:46 +0100

    Update year in copyright notices

--- mx.graalsqueak/utils.sh
+++ mx.graalsqueak/utils.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2017-2019 Software Architecture Group, Hasso Plattner Institute
+# Copyright (c) 2017-2020 Software Architecture Group, Hasso Plattner Institute
 #
 # Licensed under the MIT License.
 #
--- src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/accessing/ContextObjectNodes.java
+++ src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/accessing/ContextObjectNodes.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2019 Software Architecture Group, Hasso Plattner Institute
+ * Copyright (c) 2017-2020 Software Architecture Group, Hasso Plattner Institute
  *
  * Licensed under the MIT License.
  */
