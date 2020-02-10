6604feca74737e34af6de104f36735b0e153ff1e
Fabio Niephaus <code@fniephaus.com>
2020-02-10 15:14:51 +0100

    Send DNU if necessary as part of invokeMember
    and update in-image code for the corresponding test

--- src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/model/AbstractSqueakObject.java
+++ src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/model/AbstractSqueakObject.java
@@ -6,10 +6,13 @@
 package de.hpi.swa.graal.squeak.model;
 
 import com.oracle.truffle.api.CompilerAsserts;
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Cached.Exclusive;
 import com.oracle.truffle.api.dsl.Cached.Shared;
+import com.oracle.truffle.api.dsl.CachedContext;
+import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.interop.ArityException;
 import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.TruffleObject;
@@ -17,11 +20,16 @@ import com.oracle.truffle.api.interop.UnknownIdentifierException;
 import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
+import com.oracle.truffle.api.nodes.ControlFlowException;
 
+import de.hpi.swa.graal.squeak.SqueakLanguage;
+import de.hpi.swa.graal.squeak.exceptions.ProcessSwitch;
+import de.hpi.swa.graal.squeak.image.SqueakImageContext;
 import de.hpi.swa.graal.squeak.interop.InteropArray;
 import de.hpi.swa.graal.squeak.interop.LookupMethodByStringNode;
 import de.hpi.swa.graal.squeak.interop.WrapToSqueakNode;
 import de.hpi.swa.graal.squeak.nodes.DispatchUneagerlyNode;
+import de.hpi.swa.graal.squeak.nodes.accessing.AbstractPointersObjectNodes.AbstractPointersObjectWriteNode;
 import de.hpi.swa.graal.squeak.nodes.accessing.SqueakObjectClassNode;
 import de.hpi.swa.graal.squeak.util.ArrayUtils;
 
@@ -77,23 +85,69 @@ public abstract class AbstractSqueakObject implements TruffleObject {
     }
 
     @ExportMessage
-    public Object invokeMember(final String member, final Object[] arguments,
-                    @Shared("lookupNode") @Cached final LookupMethodByStringNode lookupNode,
-                    @Shared("classNode") @Cached final SqueakObjectClassNode classNode,
-                    @Exclusive @Cached final WrapToSqueakNode wrapNode,
-                    @Exclusive @Cached final DispatchUneagerlyNode dispatchNode) throws UnsupportedMessageException, ArityException {
-        final Object methodObject = lookupNode.executeLookup(classNode.executeLookup(this), toSelector(member));
-        if (methodObject instanceof CompiledMethodObject) {
+    public static class InvokeMember {
+        @Specialization(rewriteOn = RespecializeException.class)
+        protected static final Object invokeMember(final AbstractSqueakObject receiver, final String member, final Object[] arguments,
+                        @Shared("lookupNode") @Cached final LookupMethodByStringNode lookupNode,
+                        @Shared("classNode") @Cached final SqueakObjectClassNode classNode,
+                        @Exclusive @Cached final WrapToSqueakNode wrapNode,
+                        @Exclusive @Cached final DispatchUneagerlyNode dispatchNode) throws ArityException {
+            final Object methodObject = lookupNode.executeLookup(classNode.executeLookup(receiver), toSelector(member));
+            if (methodObject == null) {
+                CompilerDirectives.transferToInterpreter();
+                /* DoesNotUnderstand, rewrite this specialization. */
+                throw new RespecializeException();
+            }
             final CompiledMethodObject method = (CompiledMethodObject) methodObject;
             final int actualArity = arguments.length;
             final int expectedArity = method.getNumArgs();
             if (actualArity == expectedArity) {
-                return dispatchNode.executeDispatch(method, ArrayUtils.copyWithFirst(wrapNode.executeObjects(arguments), this), NilObject.SINGLETON);
+                return dispatchNode.executeDispatch(method, ArrayUtils.copyWithFirst(wrapNode.executeObjects(arguments), receiver), NilObject.SINGLETON);
             } else {
-                throw ArityException.create(1 + expectedArity, 1 + actualArity);  // +1 for receiver
+                throw ArityException.create(1 + expectedArity, 1 + actualArity);
             }
-        } else {
-            throw UnsupportedMessageException.create();
+        }
+
+        protected static final class RespecializeException extends ControlFlowException {
+            private static final long serialVersionUID = 1L;
+        }
+
+        @Specialization(replaces = "invokeMember")
+        protected static final Object invokeMemberWithDNU(final AbstractSqueakObject receiver, final String member, final Object[] arguments,
+                        @Shared("lookupNode") @Cached final LookupMethodByStringNode lookupNode,
+                        @Shared("classNode") @Cached final SqueakObjectClassNode classNode,
+                        @Exclusive @Cached final WrapToSqueakNode wrapNode,
+                        @Exclusive @Cached final DispatchUneagerlyNode dispatchNode,
+                        @Exclusive @Cached final AbstractPointersObjectWriteNode writeNode,
+                        @CachedContext(SqueakLanguage.class) final SqueakImageContext image) throws UnsupportedMessageException, ArityException {
+            final Object methodObject = lookupNode.executeLookup(classNode.executeLookup(receiver), toSelector(member));
+            if (methodObject instanceof CompiledMethodObject) {
+                final CompiledMethodObject method = (CompiledMethodObject) methodObject;
+                final int actualArity = arguments.length;
+                final int expectedArity = method.getNumArgs();
+                if (actualArity == expectedArity) {
+                    return dispatchNode.executeDispatch(method, ArrayUtils.copyWithFirst(wrapNode.executeObjects(arguments), receiver), NilObject.SINGLETON);
+                } else {
+                    throw ArityException.create(1 + expectedArity, 1 + actualArity);
+                }
+            } else {
+                final CompiledMethodObject doesNotUnderstandMethodObject = (CompiledMethodObject) lookupNode.executeLookup(classNode.executeLookup(receiver), "doesNotUnderstand:");
+                final NativeObject symbol = (NativeObject) image.asByteString(toSelector(member)).send("asSymbol");
+                final PointersObject message = image.newMessage(writeNode, symbol, classNode.executeLookup(receiver), arguments);
+                try {
+                    return dispatchNode.executeDispatch(doesNotUnderstandMethodObject, new Object[]{receiver, message}, NilObject.SINGLETON);
+                } catch (final ProcessSwitch ps) {
+                    /*
+                     * A ProcessSwitch exception is thrown in case Squeak/Smalltalk wants to open
+                     * the debugger. This needs to be avoided in headless mode.
+                     */
+                    if (image.isHeadless()) {
+                        throw UnsupportedMessageException.create();
+                    } else {
+                        throw ps;
+                    }
+                }
+            }
         }
     }
 
