1b7d19c760b1a2741710b99c5f091c976a23817e
Fabio Niephaus <code@fniephaus.com>
2020-02-26 10:37:28 +0100

    Add two experimental primitives

--- src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/plugins/GraalSqueakPlugin.java
+++ src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/plugins/GraalSqueakPlugin.java
@@ -7,13 +7,23 @@ package de.hpi.swa.graal.squeak.nodes.plugins;
 
 import java.util.List;
 
+import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.CachedContext;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.NodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.profiles.BranchProfile;
 
+import de.hpi.swa.graal.squeak.SqueakLanguage;
+import de.hpi.swa.graal.squeak.exceptions.PrimitiveExceptions.PrimitiveFailed;
+import de.hpi.swa.graal.squeak.image.SqueakImageContext;
+import de.hpi.swa.graal.squeak.model.CompiledCodeObject;
 import de.hpi.swa.graal.squeak.model.CompiledMethodObject;
+import de.hpi.swa.graal.squeak.model.NativeObject;
+import de.hpi.swa.graal.squeak.model.NilObject;
 import de.hpi.swa.graal.squeak.nodes.primitives.AbstractPrimitiveFactoryHolder;
 import de.hpi.swa.graal.squeak.nodes.primitives.AbstractPrimitiveNode;
+import de.hpi.swa.graal.squeak.nodes.primitives.PrimitiveInterfaces.BinaryPrimitive;
 import de.hpi.swa.graal.squeak.nodes.primitives.PrimitiveInterfaces.BinaryPrimitiveWithoutFallback;
 import de.hpi.swa.graal.squeak.nodes.primitives.SqueakPrimitive;
 
@@ -35,6 +45,42 @@ public final class GraalSqueakPlugin extends AbstractPrimitiveFactoryHolder {
         protected Object printArgs(final Object receiver, final Object value) {
             method.image.printToStdOut(value);
             return receiver;
+        }
+    }
+
+    @GenerateNodeFactory
+    @SqueakPrimitive(names = "primitiveLookupGraalSymbol")
+    protected abstract static class PrimLookupGraalSymbolNode extends AbstractPrimitiveNode implements BinaryPrimitive {
+        protected PrimLookupGraalSymbolNode(final CompiledMethodObject method) {
+            super(method);
+        }
+
+        @Specialization(guards = {"value.isByteType()"})
+        protected final Object doLookupGraalSymbol(@SuppressWarnings("unused") final Object receiver, final NativeObject value,
+                        @CachedContext(SqueakLanguage.class) final SqueakImageContext image,
+                        @Cached final BranchProfile errorProfile) {
+            final String symbolName = value.asStringUnsafe();
+            try {
+                final Class<?> symbolClass = Class.forName(symbolName, true, method.getCallTarget().getClass().getClassLoader());
+                return NilObject.nullToNil(image.env.asHostSymbol(symbolClass));
+            } catch (final ClassNotFoundException e) {
+                errorProfile.enter();
+                throw PrimitiveFailed.GENERIC_ERROR;
+            }
+        }
+    }
+
+    @GenerateNodeFactory
+    @SqueakPrimitive(names = "primitiveGetCallTarget")
+    protected abstract static class PrimGetCallTargetNode extends AbstractPrimitiveNode implements BinaryPrimitive {
+        protected PrimGetCallTargetNode(final CompiledMethodObject method) {
+            super(method);
+        }
+
+        @Specialization
+        protected static final Object doGet(@SuppressWarnings("unused") final Object receiver, final CompiledCodeObject code,
+                        @CachedContext(SqueakLanguage.class) final SqueakImageContext image) {
+            return image.env.asGuestValue(code.getCallTarget());
         }
     }
 }
