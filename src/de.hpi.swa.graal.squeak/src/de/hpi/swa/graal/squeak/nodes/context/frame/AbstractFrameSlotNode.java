a4ffbb2a0bb4c3fbbcec4858370d9bc76a52d9b1
Fabio Niephaus <code@fniephaus.com>
2020-02-29 17:27:00 +0100

    Drop @ReportPolymorphism from frame slot nodes
    This appears to trigger too much splitting.

--- src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/context/frame/AbstractFrameSlotNode.java
+++ src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/context/frame/AbstractFrameSlotNode.java
@@ -6,12 +6,10 @@
 package de.hpi.swa.graal.squeak.nodes.context.frame;
 
 import com.oracle.truffle.api.dsl.NodeField;
-import com.oracle.truffle.api.dsl.ReportPolymorphism;
 import com.oracle.truffle.api.frame.FrameSlot;
 
 import de.hpi.swa.graal.squeak.nodes.AbstractNode;
 
-@ReportPolymorphism
 @NodeField(name = "slot", type = FrameSlot.class)
 public abstract class AbstractFrameSlotNode extends AbstractNode {
     protected abstract FrameSlot getSlot();
