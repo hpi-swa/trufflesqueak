8f0eadfda559ec121e18799ce8a65d764c8a6b24
Fabio Niephaus <code@fniephaus.com>
2020-03-26 07:55:44 +0100

    Use LabsJDK11/OpenJDK8-JVMCI on Windows
    and refactor ci.yml as well as utils.sh
    
    - Share more code between build jobs
    - Make apt-get install quiet

--- mx.graalsqueak/utils.sh
+++ mx.graalsqueak/utils.sh
@@ -32,6 +32,10 @@ JAVA_HOME_SUFFIX="" && [[ "${OS_NAME}" == "darwin" ]] && JAVA_HOME_SUFFIX="/Cont
 readonly OS_NAME JAVA_HOME_SUFFIX
 
 
+add-path() {
+  echo "::add-path::$(resolve-path $1)"
+}
+
 deploy-asset() {
   if ! [[ "$1" =~ ^refs\/tags\/[[:digit:]] ]]; then
     echo "Skipping deployment step (ref does not start with a digit)"
@@ -85,49 +89,8 @@ download-asset() {
   local git_tag=$2
   local target="${3:-$1}"
 
-  curl -L --retry 3 --retry-connrefused --retry-delay 2 -o "${target}" \
+  curl -s -L --retry 3 --retry-connrefused --retry-delay 2 -o "${target}" \
     "https://github.com/${GITHUB_SLUG}/releases/download/${git_tag}/${filename}"
-}
-
-download-graalvm-ce() {
-  local java_version=$1
-  local target_dir=$2
-  local file_suffix=".tar.gz" && [[ "${OS_NAME}" == "windows" ]]  && file_suffix=".zip"
-  local file="graalvm-ce-${java_version}-${OS_NAME}-amd64-${DEP_GRAALVM}${file_suffix}"
-
-  pushd "${target_dir}" > /dev/null
-
-  curl -sSL --retry 3 -o "${file}" "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-${DEP_GRAALVM}/${file}"
-  if [[ "${OS_NAME}" == "windows" ]]; then unzip -qq "${file}"; else tar -xzf "${file}"; fi
-  echo "$(pwd)/graalvm-ce-${java_version}-${DEP_GRAALVM}${JAVA_HOME_SUFFIX}"
-
-  popd > /dev/null
-}
-
-download-labsjdk11() {
-  local target_dir=$1
-  local jdk_tar=${target_dir}/jdk.tar.gz
-
-  pushd "${target_dir}" > /dev/null
-
-  curl -sSL --retry 3 -o "${jdk_tar}" "https://github.com/graalvm/labs-openjdk-11/releases/download/${DEP_JVMCI}/labsjdk-ce-${DEP_JDK11}+${DEP_JDK11_UPDATE}-${DEP_JVMCI}-${OS_NAME}-amd64.tar.gz"
-  tar xzf "${jdk_tar}"
-  echo "$(pwd)/labsjdk-ce-${DEP_JDK11}-${DEP_JVMCI}${JAVA_HOME_SUFFIX}"
-
-  popd > /dev/null
-}
-
-download-openjdk8-jvmci() {
-  local target_dir=$1
-  local jdk_tar=${target_dir}/jdk.tar.gz
-
-  pushd "${target_dir}" > /dev/null
-
-  curl -sSL --retry 3 -o "${jdk_tar}" "https://github.com/graalvm/openjdk8-jvmci-builder/releases/download/${DEP_JVMCI}/openjdk-8u${DEP_JDK8_UPDATE}-${DEP_JVMCI}-${OS_NAME}-amd64.tar.gz"
-  tar xzf "${jdk_tar}"
-  echo "$(pwd)/openjdk1.8.0_${DEP_JDK8_UPDATE}-${DEP_JVMCI}${JAVA_HOME_SUFFIX}"
-
-  popd > /dev/null
 }
 
 download-graalsqueak-image() {
@@ -136,10 +99,17 @@ download-graalsqueak-image() {
   pushd "${target_dir}" > /dev/null
 
   download-asset "${DEP_IMAGE}" "${DEP_IMAGE_TAG}"
-  unzip "${DEP_IMAGE}"
+  unzip -qq "${DEP_IMAGE}"
   rm -f "${DEP_IMAGE}"
 
   popd > /dev/null
+
+  echo "[Test image downloaded successfully]"
+}
+
+enable-jdk() {
+  add-path "$1/bin"
+  set-env "JAVA_HOME" "$(resolve-path $1)"
 }
 
 ensure-test-image() {
@@ -153,11 +123,13 @@ ensure-test-image() {
   pushd "${target_dir}" > /dev/null
 
   download-asset "${DEP_TEST_IMAGE}" "${DEP_TEST_IMAGE_TAG}"
-  unzip "${DEP_TEST_IMAGE}"
+  unzip -qq "${DEP_TEST_IMAGE}"
   mv ./*.image test-64bit.image
   mv ./*.changes test-64bit.changes
 
   popd > /dev/null
+
+  echo "[Test image downloaded successfully]"
 }
 
 installable-filename() {
@@ -166,6 +138,99 @@ installable-filename() {
   local git_short_commit=$(git log -1 --format="%h")
   local git_description="${git_describe:-${git_short_commit}}"
   echo "graalsqueak-installable-${java_version}-${OS_NAME}-amd64-${git_description}-for-GraalVM-${DEP_GRAALVM}.jar"
+}
+
+resolve-path() {
+  if [[ "${OS_NAME}" == "windows" ]]; then
+    # Convert Unix path to Windows path
+    echo $1 | sed 's/\/c/C:/g' | sed 's/\//\\/g'
+  else
+    echo $1
+  fi
+}
+
+set-env() {
+  echo "::set-env name=$1::$2"
+}
+
+set-up-dependencies() {
+  local java_version=$1
+
+  set-up-mx
+  shallow-clone-graalvm-project https://github.com/oracle/graal.git
+  shallow-clone-graalvm-project https://github.com/graalvm/graaljs.git
+  download-graalsqueak-image
+  ensure-test-image
+
+  if [[ "${java_version}" == "java8" ]]; then
+    set-up-openjdk8-jvmci "${HOME}"
+  else 
+    set-up-labsjdk11 "${HOME}"
+  fi
+
+  set-up-graalvm-ce "${java_version}" "${HOME}"
+
+  set-env "INSTALLABLE_TARGET" "$(installable-filename "${java_version}")"
+}
+
+set-up-graalvm-ce() {
+  local java_version=$1
+  local target_dir=$2
+  local file_suffix=".tar.gz" && [[ "${OS_NAME}" == "windows" ]]  && file_suffix=".zip"
+  local file="graalvm-ce-${java_version}-${OS_NAME}-amd64-${DEP_GRAALVM}${file_suffix}"
+
+  pushd "${target_dir}" > /dev/null
+
+  curl -sSL --retry 3 -o "${file}" "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-${DEP_GRAALVM}/${file}"
+  if [[ "${OS_NAME}" == "windows" ]]; then unzip -qq "${file}"; else tar -xzf "${file}"; fi
+
+  popd > /dev/null
+
+  graalvm_home="${target_dir}/graalvm-ce-${java_version}-${DEP_GRAALVM}${JAVA_HOME_SUFFIX}"
+  add-path "${graalvm_home}/bin"
+  set-env "GRAALVM_HOME" "$(resolve-path ${graalvm_home})"
+  
+  echo "[${file} set up successfully]"
+}
+
+set-up-labsjdk11() {
+  local target_dir=$1
+  local jdk_tar=${target_dir}/jdk.tar.gz
+  local file="labsjdk-ce-${DEP_JDK11}+${DEP_JDK11_UPDATE}-${DEP_JVMCI}-${OS_NAME}-amd64.tar.gz"
+
+  pushd "${target_dir}" > /dev/null
+
+  curl -sSL --retry 3 -o "${jdk_tar}" "https://github.com/graalvm/labs-openjdk-11/releases/download/${DEP_JVMCI}/${file}"
+  tar xzf "${jdk_tar}"
+
+  popd > /dev/null
+
+  enable-jdk "${target_dir}/labsjdk-ce-${DEP_JDK11}-${DEP_JVMCI}${JAVA_HOME_SUFFIX}"
+
+  echo "[${file} set up successfully]"
+}
+
+set-up-mx() {
+  shallow-clone "https://github.com/graalvm/mx.git" "master" "${HOME}/mx"
+  add-path "${HOME}/mx"
+  echo "[mx set up successfully]"
+}
+
+set-up-openjdk8-jvmci() {
+  local target_dir=$1
+  local jdk_tar=${target_dir}/jdk.tar.gz
+  local file="openjdk-8u${DEP_JDK8_UPDATE}-${DEP_JVMCI}-${OS_NAME}-amd64.tar.gz"
+
+  pushd "${target_dir}" > /dev/null
+
+  curl -sSL --retry 3 -o "${jdk_tar}" "https://github.com/graalvm/openjdk8-jvmci-builder/releases/download/${DEP_JVMCI}/${file}"
+  tar xzf "${jdk_tar}"
+
+  popd > /dev/null
+
+  enable-jdk "${target_dir}/openjdk1.8.0_${DEP_JDK8_UPDATE}-${DEP_JVMCI}${JAVA_HOME_SUFFIX}"
+
+  echo "[openjdk1.8.0_${DEP_JDK8_UPDATE}-${DEP_JVMCI} set up successfully]"
 }
 
 shallow-clone() {
