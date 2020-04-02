0229744d24eaa7c9aa037152587f1e04b8c6e16c
Fabio Niephaus <code@fniephaus.com>
2020-04-02 09:25:43 +0200

    Split FrameSlotReadNode into (not) clearing nodes
    This should not affect compiled code, but is better for the interpreter.

--- src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/context/frame/FrameSlotReadNode.java
+++ src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/context/frame/FrameSlotReadNode.java
@@ -15,23 +15,24 @@ import com.oracle.truffle.api.frame.FrameUtil;
 
 import de.hpi.swa.graal.squeak.model.CompiledBlockObject;
 import de.hpi.swa.graal.squeak.model.CompiledCodeObject;
+import de.hpi.swa.graal.squeak.nodes.context.frame.FrameSlotReadNodeGen.FrameSlotReadClearNodeGen;
+import de.hpi.swa.graal.squeak.nodes.context.frame.FrameSlotReadNodeGen.FrameSlotReadNoClearNodeGen;
 
 @ImportStatic(FrameSlotKind.class)
 public abstract class FrameSlotReadNode extends AbstractFrameSlotNode {
-    protected final boolean clear;
-
-    protected FrameSlotReadNode(final boolean clear) {
-        this.clear = clear;
-    }
 
     public static FrameSlotReadNode create(final FrameSlot frameSlot) {
-        return FrameSlotReadNodeGen.create(false, frameSlot);
+        return FrameSlotReadNoClearNodeGen.create(frameSlot);
     }
 
     public static FrameSlotReadNode create(final CompiledCodeObject code, final int index) {
         // Only clear stack values, not receiver, arguments, or temporary variables.
-        final boolean clear = index >= (code instanceof CompiledBlockObject ? code.getNumArgsAndCopied() : code.getNumTemps());
-        return FrameSlotReadNodeGen.create(clear, code.getStackSlot(index));
+        final int initialSP = code instanceof CompiledBlockObject ? code.getNumArgsAndCopied() : code.getNumTemps();
+        if (index >= initialSP) {
+            return FrameSlotReadClearNodeGen.create(code.getStackSlot(index));
+        } else {
+            return FrameSlotReadNoClearNodeGen.create(code.getStackSlot(index));
+        }
     }
 
     public final Object executeRead(final Frame frame) {
@@ -58,47 +59,49 @@ public abstract class FrameSlotReadNode extends AbstractFrameSlotNode {
         return FrameUtil.getDoubleSafe(frame, getSlot());
     }
 
-    @Specialization(guards = "!shouldClear()", replaces = {"readBoolean", "readLong", "readDouble"})
-    protected final Object readObject(final Frame frame) {
-        if (!frame.isObject(getSlot())) {
-            /*
-             * The FrameSlotKind has been set to Object, so from now on all writes to the slot will
-             * be Object writes. However, now we are in a frame that still has an old non-Object
-             * value. This is a slow-path operation: we read the non-Object value, and write it
-             * immediately as an Object value so that we do not hit this path again multiple times
-             * for the same slot of the same frame.
-             */
-            CompilerDirectives.transferToInterpreter();
-            final Object value = frame.getValue(getSlot());
-            assert value != null : "Unexpected `null` value";
-            frame.setObject(getSlot(), value);
+    protected abstract static class FrameSlotReadNoClearNode extends FrameSlotReadNode {
+
+        @Specialization(replaces = {"readBoolean", "readLong", "readDouble"})
+        protected final Object readObject(final Frame frame) {
+            if (!frame.isObject(getSlot())) {
+                /*
+                 * The FrameSlotKind has been set to Object, so from now on all writes to the slot
+                 * will be Object writes. However, now we are in a frame that still has an old
+                 * non-Object value. This is a slow-path operation: we read the non-Object value,
+                 * and write it immediately as an Object value so that we do not hit this path again
+                 * multiple times for the same slot of the same frame.
+                 */
+                CompilerDirectives.transferToInterpreter();
+                final Object value = frame.getValue(getSlot());
+                assert value != null : "Unexpected `null` value";
+                frame.setObject(getSlot(), value);
+                return value;
+            } else {
+                return FrameUtil.getObjectSafe(frame, getSlot());
+            }
+        }
+    }
+
+    protected abstract static class FrameSlotReadClearNode extends FrameSlotReadNode {
+
+        @Specialization(replaces = {"readBoolean", "readLong", "readDouble"})
+        protected final Object readAndClearObject(final Frame frame) {
+            final Object value;
+            if (!frame.isObject(getSlot())) {
+                /*
+                 * The FrameSlotKind has been set to Object, so from now on all writes to the slot
+                 * will be Object writes. However, now we are in a frame that still has an old
+                 * non-Object value. This is a slow-path operation: we read the non-Object value,
+                 * and clear it immediately as an Object value so that we do not hit this path again
+                 * multiple times for the same slot of the same frame.
+                 */
+                CompilerDirectives.transferToInterpreter();
+                value = frame.getValue(getSlot());
+            } else {
+                value = FrameUtil.getObjectSafe(frame, getSlot());
+            }
+            frame.setObject(getSlot(), null);
             return value;
-        } else {
-            return FrameUtil.getObjectSafe(frame, getSlot());
         }
-    }
-
-    @Specialization(guards = "shouldClear()", replaces = {"readBoolean", "readLong", "readDouble"})
-    protected final Object readAndClearObject(final Frame frame) {
-        final Object value;
-        if (!frame.isObject(getSlot())) {
-            /*
-             * The FrameSlotKind has been set to Object, so from now on all writes to the slot will
-             * be Object writes. However, now we are in a frame that still has an old non-Object
-             * value. This is a slow-path operation: we read the non-Object value, and clear it
-             * immediately as an Object value so that we do not hit this path again multiple times
-             * for the same slot of the same frame.
-             */
-            CompilerDirectives.transferToInterpreter();
-            value = frame.getValue(getSlot());
-        } else {
-            value = FrameUtil.getObjectSafe(frame, getSlot());
-        }
-        frame.setObject(getSlot(), null);
-        return value;
-    }
-
-    protected final boolean shouldClear() {
-        return clear; // Work around "Useless condition" error in generated code.
     }
 }
