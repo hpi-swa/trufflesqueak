4b8ed001e8c5128de1e0b9e7de66105aaaafef0f
Fabio Niephaus <code@fniephaus.com>
2020-05-26 12:59:03 +0200

    Debug

--- mx.trufflesqueak/mx_trufflesqueak.py
+++ mx.trufflesqueak/mx_trufflesqueak.py
@@ -560,5 +560,4 @@ if mx.is_windows():
     import ssl
     ssl._create_default_https_context = ssl._create_unverified_context
 
-print(os.environ)
-os.system('dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.22.27905\bin\HostX64\x64"')
+os.system('dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\"')
