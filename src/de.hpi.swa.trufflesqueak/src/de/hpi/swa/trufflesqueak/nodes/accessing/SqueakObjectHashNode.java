02e4b5d9ea786d5e4d26a337d2a05ddac3b21e83
Fabio Niephaus <code@fniephaus.com>
2020-06-15 16:29:20 +0200

    Add TruffleObject spec to SqueakObjectHashNode
    Since c0ec3c2, this specialization should not be needed (see #119). However, we might need this when the identity APIs are introduced (see 20.2 changelog).

--- src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/accessing/SqueakObjectHashNode.java
+++ src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/accessing/SqueakObjectHashNode.java
@@ -7,6 +7,7 @@ package de.hpi.swa.trufflesqueak.nodes.accessing;
 
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.nodes.NodeCost;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import com.oracle.truffle.api.profiles.BranchProfile;
@@ -45,5 +46,10 @@ public abstract class SqueakObjectHashNode extends AbstractNode {
     protected static final long doAbstractSqueakObjectWithHash(final AbstractSqueakObjectWithHash object,
                     @Cached final BranchProfile needsHashProfile) {
         return object.getSqueakHash(needsHashProfile);
+    }
+
+    @Specialization(guards = {"!isAbstractSqueakObject(value)"})
+    protected static final long doForeignObject(final TruffleObject value) {
+        return System.identityHashCode(value);
     }
 }
