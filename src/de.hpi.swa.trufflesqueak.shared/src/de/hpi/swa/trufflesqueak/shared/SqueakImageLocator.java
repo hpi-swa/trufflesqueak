7eaeee5b36b5ee0fcac28986c40c4c34a78d41fc
Fabio Niephaus <code@fniephaus.com>
2020-06-16 13:05:27 +0200

    Avoid NPE in SqueakImageLocator

--- src/de.hpi.swa.trufflesqueak.shared/src/de/hpi/swa/trufflesqueak/shared/SqueakImageLocator.java
+++ src/de.hpi.swa.trufflesqueak.shared/src/de/hpi/swa/trufflesqueak/shared/SqueakImageLocator.java
@@ -15,7 +15,11 @@ public final class SqueakImageLocator {
 
     /* Returns path to image file in TruffleSqueak's resources directory. */
     public static String findImage() {
-        final Path resourcesDirectory = HomeFinder.getInstance().getLanguageHomes().get(SqueakLanguageConfig.ID).resolve("resources");
+        final Path languageHome = HomeFinder.getInstance().getLanguageHomes().get(SqueakLanguageConfig.ID);
+        if (languageHome == null) {
+            throw new RuntimeException("Unable to locate TruffleSqueak's language home.");
+        }
+        final Path resourcesDirectory = languageHome.resolve("resources");
         final File resourcesDirectoryFile = resourcesDirectory.toFile();
         final String[] imageFiles = resourcesDirectoryFile.list((dir, name) -> dir == resourcesDirectoryFile && name.endsWith(".image"));
         if (imageFiles != null && imageFiles.length > 0) {
