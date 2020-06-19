e6d6afc3770f0e9e5cc3be9aac5c74de99e0ee68
Fabio Niephaus <code@fniephaus.com>
2020-06-19 10:17:36 +0200

    Start next release cycle

--- src/de.hpi.swa.trufflesqueak.shared/src/de/hpi/swa/trufflesqueak/shared/SqueakLanguageConfig.java
+++ src/de.hpi.swa.trufflesqueak.shared/src/de/hpi/swa/trufflesqueak/shared/SqueakLanguageConfig.java
@@ -11,7 +11,7 @@ public final class SqueakLanguageConfig {
     public static final String MIME_TYPE = "application/x-smalltalk";
     public static final String NAME = "Squeak/Smalltalk";
     public static final String ST_MIME_TYPE = "text/x-smalltalk";
-    public static final String VERSION = "20.1.0";
+    public static final String VERSION = "20.2.0-dev";
 
     private SqueakLanguageConfig() {
     }
