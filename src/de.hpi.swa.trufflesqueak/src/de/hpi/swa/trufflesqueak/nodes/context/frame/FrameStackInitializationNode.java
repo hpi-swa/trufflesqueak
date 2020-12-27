5004c495e73f6c0a4528daee87453f983fd492ad
Fabio Niephaus <code@fniephaus.com>
2020-12-27 19:17:46 +0100

    Do not initialize temps with nil

--- src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/context/frame/FrameStackInitializationNode.java
+++ src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/context/frame/FrameStackInitializationNode.java
@@ -5,7 +5,6 @@
  */
 package de.hpi.swa.trufflesqueak.nodes.context.frame;
 
-import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.frame.FrameSlot;
@@ -13,15 +12,12 @@ import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
 
 import de.hpi.swa.trufflesqueak.model.BlockClosureObject;
-import de.hpi.swa.trufflesqueak.model.CompiledCodeObject;
-import de.hpi.swa.trufflesqueak.model.NilObject;
 import de.hpi.swa.trufflesqueak.nodes.AbstractNode;
 import de.hpi.swa.trufflesqueak.util.FrameAccess;
 
 public final class FrameStackInitializationNode extends AbstractNode {
     @CompilationFinal private FrameSlot stackPointerSlot;
-    @CompilationFinal private int numArgs;
-    @Children private FrameSlotWriteNode[] writeNodes;
+    @CompilationFinal private int initialSP;
 
     public static FrameStackInitializationNode create() {
         return new FrameStackInitializationNode();
@@ -32,31 +28,13 @@ public final class FrameStackInitializationNode extends AbstractNode {
         if (stackPointerSlot == null) {
             CompilerDirectives.transferToInterpreterAndInvalidate();
             stackPointerSlot = FrameAccess.getStackPointerSlot(frame);
-            final CompiledCodeObject code;
-            final int initialSP;
             final BlockClosureObject closure = FrameAccess.getClosure(frame);
             if (closure == null) {
-                code = FrameAccess.getCodeObject(frame);
-                initialSP = code.getNumTemps();
-                numArgs = code.getNumArgs();
+                initialSP = FrameAccess.getCodeObject(frame).getNumTemps();
             } else {
-                code = closure.getCompiledBlock();
-                initialSP = closure.getNumTemps();
-                numArgs = (int) (closure.getNumArgs() + closure.getNumCopied());
-            }
-            writeNodes = new FrameSlotWriteNode[initialSP];
-            for (int i = numArgs; i < writeNodes.length; i++) {
-                writeNodes[i] = insert(FrameSlotWriteNode.create(frame, i));
+                initialSP = (int) (closure.getNumArgs() + closure.getNumCopied());
             }
         }
-        CompilerAsserts.partialEvaluationConstant(writeNodes.length);
-        final Object[] arguments = frame.getArguments();
-        assert arguments.length == FrameAccess.expectedArgumentSize(numArgs);
-        // FIXME: avoid generalizing temp slots.
-        // Initialize remaining temporary variables with nil in newContext.
-        for (int i = numArgs; i < writeNodes.length; i++) {
-            writeNodes[i].executeWrite(frame, NilObject.SINGLETON);
-        }
-        FrameAccess.setStackPointer(frame, stackPointerSlot, writeNodes.length);
+        FrameAccess.setStackPointer(frame, stackPointerSlot, initialSP);
     }
 }
