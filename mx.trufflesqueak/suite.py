6d09fae66273d59ee56058a411fed674533218b4
Fabio Niephaus <code@fniephaus.com>
2022-10-30 11:16:56 +0100

    Start next release cycle [ci skip]

--- mx.trufflesqueak/suite.py
+++ mx.trufflesqueak/suite.py
@@ -14,7 +14,7 @@ suite = {
     "mxversion": "6.9.2",
     "versionConflictResolution": "latest",
 
-    "version": "22.3.0",
+    "version": "23.0.0",
     "trufflesqueak:dependencyMap": {
         "cuis_test_image": "CuisTestImage-6.0-5053.zip",
         "cuis_test_image_tag": "21.3.0",
@@ -24,7 +24,7 @@ suite = {
         "test_image_tag": "21.1.0",
     },
 
-    "release": True,
+    "release": False,
     "groupId": "de.hpi.swa.trufflesqueak",
     "url": "https://github.com/hpi-swa/trufflesqueak",
 
