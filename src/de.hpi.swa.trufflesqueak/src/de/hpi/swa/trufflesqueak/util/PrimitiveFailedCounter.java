368d49e050338c82899e66338448943b43817175
Fabio Niephaus <code@fniephaus.com>
2023-02-12 16:58:56 +0100

    Add time window to `PrimitiveFailedCounter`

--- src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/util/PrimitiveFailedCounter.java
+++ src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/util/PrimitiveFailedCounter.java
@@ -6,13 +6,18 @@
  */
 package de.hpi.swa.trufflesqueak.util;
 
+import com.oracle.truffle.api.CompilerAsserts;
+
 /*
- * Counts how often a primitive has failed and indicates whether this node should continue to
- * send the primitive eagerly or not. This is useful to avoid rewriting primitives that set up
- * the image and then are retried in their fallback code (e.g. primitiveCopyBits).
+ * Counts how often a primitive has failed in a certain time window and indicates whether this
+ * node should continue to send the primitive eagerly or not. This is useful to avoid
+ * rewriting primitives that set up the image and then are retried in their fallback code
+ * (e.g. primitiveCopyBits).
  */
 public final class PrimitiveFailedCounter {
-    private static final int PRIMITIVE_FAILED_THRESHOLD = 3;
+    private static final int MAX_NUM_FAILURES = 3;
+    private static final int TIME_WINDOW_MILLIS = 1_000;
+    private long lastCheckMillis = System.currentTimeMillis();
     private int count;
 
     public static PrimitiveFailedCounter create() {
@@ -20,6 +25,11 @@ public final class PrimitiveFailedCounter {
     }
 
     public boolean shouldNoLongerSendEagerly() {
-        return ++count > PRIMITIVE_FAILED_THRESHOLD;
+        CompilerAsserts.neverPartOfCompilation();
+        if (System.currentTimeMillis() - lastCheckMillis > TIME_WINDOW_MILLIS) {
+            count = 0; // reset
+        }
+        lastCheckMillis = System.currentTimeMillis();
+        return ++count > MAX_NUM_FAILURES;
     }
 }
