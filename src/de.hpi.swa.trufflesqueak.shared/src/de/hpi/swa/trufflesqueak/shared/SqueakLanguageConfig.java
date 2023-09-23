c68ce192c4e9caaf82fac9d35699568f89dedf57
Fabio Niephaus <code@fniephaus.com>
2023-09-23 14:58:39 +0200

    Update image list.

--- src/de.hpi.swa.trufflesqueak.shared/src/de/hpi/swa/trufflesqueak/shared/SqueakLanguageConfig.java
+++ src/de.hpi.swa.trufflesqueak.shared/src/de/hpi/swa/trufflesqueak/shared/SqueakLanguageConfig.java
@@ -17,9 +17,9 @@ public final class SqueakLanguageConfig {
     public static final String WEBSITE = "https://github.com/hpi-swa/trufflesqueak";
 
     public static final String[][] SUPPORTED_IMAGES = {
-                    {"TruffleSqueak image (23.0.1) (recommended)", "https://github.com/hpi-swa/trufflesqueak/releases/download/23.0.1/TruffleSqueakImage-23.0.1.zip"},
+                    {"TruffleSqueak image (23.1.0) (recommended)", "https://github.com/hpi-swa/trufflesqueak/releases/download/23.1.0/TruffleSqueakImage-23.1.0.zip"},
                     {"Squeak/Smalltalk (6.0-22104)", "https://files.squeak.org/6.0/Squeak6.0-22104-64bit/Squeak6.0-22104-64bit.zip"},
-                    {"TruffleSqueak test image (6.0alpha-20228b)", "https://github.com/hpi-swa/trufflesqueak/releases/download/21.1.0/TruffleSqueakTestImage-6.0alpha-20228b-64bit.zip"},
+                    {"TruffleSqueak test image (6.0-22104)", "https://github.com/hpi-swa/trufflesqueak/releases/download/22.3.0/TruffleSqueakTestImage-6.0-22104-64bit.zip"},
                     {"Cuis-Smalltalk test image (6.0-5053)", "https://github.com/hpi-swa/trufflesqueak/releases/download/21.3.0/CuisTestImage-6.0-5053.zip"}};
 
     private SqueakLanguageConfig() {
