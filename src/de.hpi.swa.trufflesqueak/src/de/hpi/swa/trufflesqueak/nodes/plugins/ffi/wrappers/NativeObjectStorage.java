8c9b050cb24c139293adb451cd4154fbc6316e89
Malte Stellmacher <malte.stellmacher@student.hpi.de>
2023-11-16 14:15:49 +0100

    Do not assume ByteStorage

--- src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/plugins/ffi/wrappers/NativeObjectStorage.java
+++ src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/plugins/ffi/wrappers/NativeObjectStorage.java
@@ -4,13 +4,28 @@ import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
+import de.hpi.swa.trufflesqueak.model.NativeObject;
 import de.hpi.swa.trufflesqueak.nodes.plugins.ffi.InterpreterProxy.PostPrimitiveCleanup;
-import de.hpi.swa.trufflesqueak.util.UnsafeUtils;
 
 @ExportLibrary(InteropLibrary.class)
 public abstract class NativeObjectStorage implements PostPrimitiveCleanup, TruffleObject {
     protected long nativeAddress;
     private boolean isAllocated = false;
+
+    public static NativeObjectStorage from(NativeObject object) {
+        if (object.isByteType()) {
+            return new ByteStorage(object.getByteStorage());
+        } else if (object.isIntType()) {
+            return new IntStorage(object.getIntStorage());
+        } else if (object.isLongType()) {
+            return new LongStorage(object.getLongStorage());
+        } else if (object.isShortType()) {
+            return new ShortStorage(object.getShortStorage());
+        } else {
+            throw new IllegalArgumentException("Object storage type is not supported.");
+        }
+    }
+
 
     @ExportMessage
     public boolean isPointer() {
