d6f8161d5d374805cba14e8cdf23a128a366e919
Fabio Niephaus <code@fniephaus.com>
2023-12-17 18:35:59 +0100

    Disable interrupts during interop sends

--- src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/model/AbstractSqueakObject.java
+++ src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/model/AbstractSqueakObject.java
@@ -14,6 +14,9 @@ import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Bind;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Cached.Shared;
+import com.oracle.truffle.api.dsl.GenerateCached;
+import com.oracle.truffle.api.dsl.GenerateInline;
+import com.oracle.truffle.api.dsl.GenerateUncached;
 import com.oracle.truffle.api.dsl.ReportPolymorphism;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.interop.InteropLibrary;
@@ -31,6 +34,7 @@ import com.oracle.truffle.api.nodes.Node;
 import de.hpi.swa.trufflesqueak.exceptions.ProcessSwitch;
 import de.hpi.swa.trufflesqueak.image.SqueakImageContext;
 import de.hpi.swa.trufflesqueak.interop.WrapToSqueakNode;
+import de.hpi.swa.trufflesqueak.nodes.AbstractNode;
 import de.hpi.swa.trufflesqueak.nodes.LookupMethodNode;
 import de.hpi.swa.trufflesqueak.nodes.accessing.SqueakObjectClassNode;
 import de.hpi.swa.trufflesqueak.nodes.dispatch.DispatchUneagerlyNode;
@@ -56,18 +60,37 @@ public abstract class AbstractSqueakObject implements TruffleObject {
     }
 
     @ExportMessage
-    protected static class Send {
+    protected static final Object send(final AbstractSqueakObject receiver, final Message message, final Object[] arguments,
+                    @SuppressWarnings("unused") @Bind("$node") final Node node,
+                    @Cached final PerformInteropSendNode performInteropSendNode) throws Exception {
+        final SqueakImageContext image = SqueakImageContext.get(node);
+        final boolean wasActive = image.interrupt.isActive();
+        image.interrupt.deactivate();
+        try {
+            return performInteropSendNode.execute(node, receiver, message, arguments);
+        } finally {
+            if (wasActive) {
+                image.interrupt.activate();
+            }
+        }
+    }
+
+    @GenerateInline
+    @GenerateUncached
+    @GenerateCached(false)
+    protected abstract static class PerformInteropSendNode extends AbstractNode {
+        protected abstract Object execute(Node node, AbstractSqueakObject receiver, Message message, Object[] arguments) throws Exception;
+
         @SuppressWarnings("unused")
         @ExplodeLoop
         @Specialization(guards = {"message == cachedMessage", "classNode.executeLookup(node, receiver) == cachedClass", "cachedMethod != null"}, limit = "8", //
                         assumptions = {"cachedClass.getClassHierarchyStable()", "cachedClass.getMethodDictStable()", "cachedMethod.getCallTargetStable()"})
-        protected static final Object doSendCached(final AbstractSqueakObject receiver, final Message message, final Object[] arguments,
-                        @SuppressWarnings("unused") @Bind("this") final Node node,
+        protected static final Object doSendCached(final Node node, final AbstractSqueakObject receiver, final Message message, final Object[] arguments,
                         @Shared("classNode") @Cached final SqueakObjectClassNode classNode,
                         @Cached("message") final Message cachedMessage,
                         @Cached("classNode.executeLookup(node, receiver)") final ClassObject cachedClass,
                         @Cached("lookupMethod(cachedClass, cachedMessage)") final CompiledCodeObject cachedMethod,
-                        @Cached("create(cachedMethod.getCallTarget())") final DirectCallNode callNode,
+                        @Cached(value = "create(cachedMethod.getCallTarget())", inline = false) final DirectCallNode callNode,
                         @Shared("wrapNode") @Cached final WrapToSqueakNode wrapNode) {
             final int numArgs = cachedMessage.getParameterCount() - 1;
             assert numArgs == arguments.length;
@@ -89,13 +112,12 @@ public abstract class AbstractSqueakObject implements TruffleObject {
         @TruffleBoundary
         @ReportPolymorphism.Megamorphic
         @Specialization(replaces = "doSendCached")
-        protected static final Object doSendGeneric(final AbstractSqueakObject receiver, final Message message, final Object[] arguments,
-                        @Bind("this") final Node node,
+        protected static final Object doSendGeneric(final Node node, final AbstractSqueakObject receiver, final Message message, final Object[] arguments,
                         @Cached final LookupMethodNode lookupNode,
                         @Shared("classNode") @Cached final SqueakObjectClassNode classNode,
                         @Cached final DispatchUneagerlyNode dispatchNode,
                         @Shared("wrapNode") @Cached final WrapToSqueakNode wrapNode) throws Exception {
-            final SqueakImageContext image = SqueakImageContext.get(lookupNode);
+            final SqueakImageContext image = SqueakImageContext.get(node);
             if (message.getLibraryClass() == InteropLibrary.class) {
                 final NativeObject selector = image.toInteropSelector(message);
                 final Object methodObject = lookupNode.executeLookup(node, classNode.executeLookup(node, receiver), selector);
@@ -120,8 +142,8 @@ public abstract class AbstractSqueakObject implements TruffleObject {
             CompilerDirectives.transferToInterpreter();
             // Fall back to other, concrete or the default library implementation
             // FIXME: avoid reflection here somehow, the following does not work:
-            // return ReflectionLibrary.getFactory().getUncached(receiver).send(DEFAULT, message,
-            // arguments);
+            // return ReflectionLibrary.getFactory().getUncached(receiver).send(DEFAULT,
+            // message, arguments);
             final LibraryFactory<?> lib = message.getFactory();
             final Method genericDispatchMethod = lib.getClass().getDeclaredMethod("genericDispatch", Library.class, Object.class, Message.class, Object[].class, int.class);
             genericDispatchMethod.setAccessible(true);
