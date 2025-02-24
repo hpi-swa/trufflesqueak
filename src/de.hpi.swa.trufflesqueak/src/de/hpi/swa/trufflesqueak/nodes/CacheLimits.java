b63865b1a71c8f11caadd9ecb6ad2052ff212385
Fabio Niephaus <code@fniephaus.com>
2025-02-24 18:30:56 +0100

    Introduce NEW_CACHE_LIMIT and reduce to 3

--- src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/CacheLimits.java
+++ src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/CacheLimits.java
@@ -11,6 +11,7 @@ public final class CacheLimits {
     public static final int INDIRECT_PRIMITIVE_CACHE_LIMIT = 2;
     public static final int INLINE_BLOCK_CACHE_LIMIT = 4;
     public static final int INLINE_METHOD_CACHE_LIMIT = 4;
+    public static final int NEW_CACHE_LIMIT = 3;
     public static final int PERFORM_SELECTOR_CACHE_LIMIT = 4;
     public static final int POINTERS_LAYOUT_CACHE_LIMIT = 4;
     public static final int POINTERS_VARIABLE_PART_CACHE_LIMIT = 3;
