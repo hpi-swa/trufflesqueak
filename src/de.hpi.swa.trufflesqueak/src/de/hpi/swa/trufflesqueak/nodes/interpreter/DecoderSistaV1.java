9a661e6f9beaf7c6c3da4ad6bd57c2afa99c6b48
Sabine Schnabeltier <sabine.schnabeltier@gmail.com>
2025-12-13 16:55:44 -0600

    style fixes

--- src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/interpreter/DecoderSistaV1.java
+++ src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/interpreter/DecoderSistaV1.java
@@ -170,20 +170,20 @@ public final class DecoderSistaV1 extends AbstractDecoder {
         int maxStackPointer = currentStackPointer;
         final int contextSize = code.getSqueakContextSize();
         // Uncomment the following and compare with `(Character>>#isSeparator) detailedSymbolic`
-//        final int initialPC = code.getInitialPC();
-//        final StringBuilder sb = new StringBuilder();
-//        sb.append(code).append("[").append(contextSize).append("]\n");
+        // final int initialPC = code.getInitialPC();
+        // final StringBuilder sb = new StringBuilder();
+        // sb.append(code).append("[").append(contextSize).append("]\n");
         while (index < trailerPosition) {
-//             sb.append(initialPC + index).append(":\t").append(currentStackPointer).append("->");
+            // sb.append(initialPC + index).append(":\t").append(currentStackPointer).append("->");
             joins[index] = currentStackPointer;
             currentStackPointer = decodeStackPointer(code, joins, index, currentStackPointer);
-//             sb.append(currentStackPointer).append("\n");
+            // sb.append(currentStackPointer).append("\n");
             assert 0 <= currentStackPointer && currentStackPointer <= contextSize;
             maxStackPointer = Math.max(maxStackPointer, currentStackPointer);
             index += decodeNextPCDelta(code, index);
         }
-//        sb.append("max SP = ").append(maxStackPointer).append("\n");
-//        System.out.append(sb.toString());
+        // sb.append("max SP = ").append(maxStackPointer).append("\n");
+        // System.out.append(sb.toString());
         assert 0 <= maxStackPointer && maxStackPointer <= contextSize;
         return maxStackPointer;
     }
