e07753be0f83a0b7ce6f6002df3b417d66df7332
Sabine Schnabeltier <34526618+Sabine-Schnabeltier@users.noreply.github.com>
2026-02-05 10:27:26 -0600

    Initialize Squeak hash when reading LargeIntegers from image (#236)
    * make sure large integers read their hash from the image
    
    ---------
    
    Co-authored-by: Fabio Niephaus <code@fniephaus.com>

--- src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/plugins/LargeIntegers.java
+++ src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/plugins/LargeIntegers.java
@@ -1086,11 +1086,15 @@ public final class LargeIntegers extends AbstractPrimitiveFactoryHolder {
 
     public static Object normalize(final SqueakImageChunk chunk, final boolean isNegative) {
         final Object object = normalize(chunk.getImage(), chunk.getBytes(), isNegative);
-        chunk.setObject(object);
+        if (object instanceof final NativeObject o) {
+            o.initializeFrom(chunk);
+        } else {
+            chunk.setObject(object);
+        }
         return object;
     }
 
-    public static Object normalize(final SqueakImageContext image, final byte[] bytes, final boolean isNegative) {
+    private static Object normalize(final SqueakImageContext image, final byte[] bytes, final boolean isNegative) {
         final int byteSize = bytes.length;
         // Compute number of 32-bit digits (rounding up)
         int digitLen = (byteSize + 3) / 4;
