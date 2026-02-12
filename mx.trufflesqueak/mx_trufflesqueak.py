0fb0d182c50fa6fd0efbbf7dbae039b0145fd196
Fabio Niephaus <code@fniephaus.com>
2026-02-12 16:44:29 +0100

    Disable G1 for now

--- mx.trufflesqueak/mx_trufflesqueak.py
+++ mx.trufflesqueak/mx_trufflesqueak.py
@@ -37,9 +37,9 @@ def libsmalltalkvm_build_args():
         else ("armv8.1-a" if mx.get_arch() == "aarch64" else "compatibility")
     )
     build_args.append(f"-march={selected_march}")
-    is_oracle_graalvm = "-community" not in os.getenv("JAVA_HOME")
-    if is_oracle_graalvm and mx.get_os() == "linux":
-        build_args.append("--gc=G1")
+    # is_oracle_graalvm = "-community" not in os.getenv("JAVA_HOME")
+    # if is_oracle_graalvm and mx.get_os() == "linux":
+    #     build_args.append("--gc=G1")
     return build_args
 
 
