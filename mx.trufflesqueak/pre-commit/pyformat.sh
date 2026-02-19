e084dcdc81f9025a4d07b01f90e0dc5eb9ca477d
Fabio Niephaus <code@fniephaus.com>
2026-02-19 10:44:19 +0100

    Add `pre-commit` config

---
+++ mx.trufflesqueak/pre-commit/pyformat.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2026-2026 Software Architecture Group, Hasso Plattner Institute
+# Copyright (c) 2026-2026 Oracle and/or its affiliates
+#
+# Licensed under the MIT License.
+#
+
+mx pyformat "${@}"
