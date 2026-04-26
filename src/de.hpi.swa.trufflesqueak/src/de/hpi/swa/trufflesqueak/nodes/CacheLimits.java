b1ea71ab1b3869367e0b35308810f0cb4c877597
Fabio Niephaus <code@fniephaus.com>
2026-04-26 19:19:40 +0200

    Align PIC size with OSVM

--- src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/CacheLimits.java
+++ src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/CacheLimits.java
@@ -9,8 +9,8 @@ package de.hpi.swa.trufflesqueak.nodes;
 public final class CacheLimits {
     public static final int EXECUTE_METHOD_CACHE_LIMIT = 4;
     public static final int INDIRECT_PRIMITIVE_CACHE_LIMIT = 2;
-    public static final int INLINE_BLOCK_CACHE_LIMIT = 4;
-    public static final int INLINE_METHOD_CACHE_LIMIT = 4;
+    public static final int INLINE_METHOD_CACHE_LIMIT = 6; // see OSVM's MaxCPICCases
+    public static final int INLINE_BLOCK_CACHE_LIMIT = INLINE_METHOD_CACHE_LIMIT;
     public static final int NEW_CACHE_LIMIT = 3;
     public static final int PERFORM_SELECTOR_CACHE_LIMIT = 4;
     public static final int POINTERS_LAYOUT_CACHE_LIMIT = 4;
