bbd99fef145e4c74f4cd0265ab44fd25f6b30549
Sabine Schnabeltier <sabine.schnabeltier@gmail.com>
2026-06-28 17:08:32 -0500

    simplify as per PR comments

--- src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/interrupts/CheckForInterruptsState.java
+++ src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/interrupts/CheckForInterruptsState.java
@@ -117,10 +117,7 @@ public final class CheckForInterruptsState {
     }
 
     private void wakeupVM() {
-        final Thread theThread = vmThread;
-        if (theThread != null) {
-            LockSupport.unpark(theThread);
-        }
+        LockSupport.unpark(vmThread);
     }
 
     /* Interrupt check interval */
